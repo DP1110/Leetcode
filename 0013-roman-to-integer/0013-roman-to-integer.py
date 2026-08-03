60d8c34d9ba46cbd50be1e804a233fe2c2b282c4
DP1110 <manedipak1110@gmail.com>
2026-08-03 22:46:12 +0530

    Time: 3 ms (93.74%), Space: 12.3 MB (57.55%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def romanToInt(self, s):
+        # Map each Roman symbol to its value
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            current = values[s[i]]
+            # If next value exists and is larger, this is a subtractive pair
+            if i + 1 < n and current < values[s[i + 1]]:
+                total -= current
+            else:
+                total += current
+        
+        return total
