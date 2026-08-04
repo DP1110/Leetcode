d52ad08a15a331b79f9736f7589a01a8cad0a43f
DP1110 <manedipak1110@gmail.com>
2026-08-04 23:12:35 +0530

    Add solution for Reverse Nodes in k-Group

--- LeetCode/Reverse-Nodes-in-k-Group/Solution.py
+++ LeetCode/Reverse-Nodes-in-k-Group/Solution.py
@@ -1,34 +1,30 @@
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6
-7class Solution:
-8    def reverseKGroup(self, head, k):
-9        dummy = ListNode(0, head)
-10        group_prev = dummy
-11        
-12        while True:
-13            # Check if there are k nodes remaining
-14            kth = group_prev
-15            for _ in range(k):
-16                kth = kth.next
-17                if not kth:
-18                    return dummy.next
-19            group_next = kth.next
+1
+2
+3class Solution:
+4    def reverseKGroup(self, head, k):
+5        dummy = ListNode(0, head)
+6        group_prev = dummy
+7        
+8        while True:
+9            # Check if there are k nodes remaining
+10            kth = group_prev
+11            for _ in range(k):
+12                kth = kth.next
+13                if not kth:
+14                    return dummy.next
+15            group_next = kth.next
+16            
+17            # Reverse the k nodes: group_prev.next ... kth
+18            prev_node = group_next   # Will become the successor of the new tail
+19            curr = group_prev.next   # First node of current group
 20            
-21            # Reverse the k nodes: group_prev.next ... kth
-22            prev_node = group_next   # Will become the successor of the new tail
-23            curr = group_prev.next   # First node of current group
-24            
-25            while curr != group_next:
-26                nxt = curr.next
-27                curr.next = prev_node
-28                prev_node = curr
-29                curr = nxt
-30            
-31            # Move group_prev to the tail of the just-reversed group
-32            temp = group_prev.next   # Old head, now tail
-33            group_prev.next = kth    # New head
-34            group_prev = temp
+21            while curr != group_next:
+22                nxt = curr.next
+23                curr.next = prev_node
+24                prev_node = curr
+25                curr = nxt
+26            
+27            # Move group_prev to the tail of the just-reversed group
+28            temp = group_prev.next   # Old head, now tail
+29            group_prev.next = kth    # New head
+30            group_prev = temp
