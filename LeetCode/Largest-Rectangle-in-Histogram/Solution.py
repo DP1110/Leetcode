cfff53472eb79c067a48c38821a90b67da42cd61
DP1110 <manedipak1110@gmail.com>
2026-08-11 18:17:39 +0530

    Add solution for Largest Rectangle in Histogram

--- LeetCode/Largest-Rectangle-in-Histogram/Solution.py
+++ LeetCode/Largest-Rectangle-in-Histogram/Solution.py
@@ -1,17 +1,17 @@
 1class Solution:
 2    def largestRectangleArea(self, heights):
-3        stack = []  # stores indices, heights are increasing
+3        stack = []  # stores indices with increasing heights
 4        max_area = 0
 5        n = len(heights)
 6        
 7        for i in range(n + 1):
-8            # Use 0 as sentinel to flush remaining bars
+8            # Sentinel 0 at the end to flush remaining bars
 9            curr_height = heights[i] if i < n else 0
 10            
 11            while stack and curr_height < heights[stack[-1]]:
 12                height = heights[stack.pop()]
-13                # If stack is empty after pop, width extends from index 0 to i-1
-14                # Otherwise, width extends from stack[-1]+1 to i-1
+13                # Width: if stack empty, spans from 0 to i-1
+14                # else spans from stack[-1]+1 to i-1
 15                width = i if not stack else i - stack[-1] - 1
 16                max_area = max(max_area, height * width)
 17            
