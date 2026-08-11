48130ce6210cf233cfd07b3de1f274ba6164ac0f
DP1110 <manedipak1110@gmail.com>
2026-08-11 18:20:55 +0530

    Add solution for Combinations

--- LeetCode/Combinations/Solution.py
+++ LeetCode/Combinations/Solution.py
@@ -1,3 +1,16 @@
-Input:	2, 2
-Output:	[[2,1]]
-Expected:	[[1,2]]
+1class Solution(object):
+2    def combine(self, n, k):
+3        res = []
+4        path = []
+5
+6        def backtrack(start):
+7            if len(path) == k:
+8                res.append(path[:])
+9                return
+10            for i in range(start, n - (k - len(path)) + 2):
+11                path.append(i)
+12                backtrack(i + 1)
+13                path.pop()
+14
+15        backtrack(1)
+16        return res
