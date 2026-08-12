6ac5b8655008f77d0e1c04b962bdb754286fc31c
DP1110 <manedipak1110@gmail.com>
2026-08-12 17:27:22 +0530

    Add solution for Decode Ways

---
+++ LeetCode/Decode-Ways/Solution.py
@@ -0,0 +1,25 @@
+1class Solution(object):
+2    def numDecodings(self, s):
+3        if not s or s[0] == '0':
+4            return 0
+5        
+6        n = len(s)
+7        prev2 = 1  # dp[i-2]
+8        prev1 = 1  # dp[i-1]
+9        
+10        for i in range(1, n):
+11            current = 0
+12            
+13            # Single digit
+14            if s[i] != '0':
+15                current += prev1
+16            
+17            # Two digits
+18            two_digit = int(s[i-1:i+1])
+19            if 10 <= two_digit <= 26:
+20                current += prev2
+21            
+22            prev2 = prev1
+23            prev1 = current
+24        
+25        return prev1
