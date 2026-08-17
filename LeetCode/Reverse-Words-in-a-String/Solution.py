7da5f723cee3b2084c924b6ff098b3b6c8e1c81a
DP1110 <manedipak1110@gmail.com>
2026-08-17 19:15:58 +0530

    Add solution for Reverse Words in a String

--- LeetCode/Reverse-Words-in-a-String/Solution.py
+++
@@ -1,9 +0,0 @@
-1class Solution(object):
-2    def reverseWords(self, s):
-3        """
-4        :type s: str
-5        :rtype: str
-6        """
-7        words = s.split()  # splits on any whitespace run, drops empties
-8        words.reverse()
-9        return ' '.join(words)
