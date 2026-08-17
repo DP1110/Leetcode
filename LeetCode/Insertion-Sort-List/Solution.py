25e6f3bcbf352a45c24921522b3c3574befa6d3c
DP1110 <manedipak1110@gmail.com>
2026-08-17 19:19:40 +0530

    Add solution for Insertion Sort List

--- LeetCode/Insertion-Sort-List/Solution.py
+++ LeetCode/Insertion-Sort-List/Solution.py
@@ -1,22 +1,19 @@
-1class Solution(object):
-2    def insertionSortList(self, head):
-3        """
-4        :type head: ListNode
-5        :rtype: ListNode
-6        """
-7        dummy = ListNode(0)
-8        cur = head
-9
-10        while cur:
-11            nxt = cur.next  # save next before rewire
-12
-13            p = dummy
-14            while p.next and p.next.val < cur.val:
-15                p = p.next
-16
-17            cur.next = p.next
-18            p.next = cur
-19
-20            cur = nxt
-21
-22        return dummy.next
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        head.next = insertionSortList(head.next);
+        return swapIfLarge(head);
+    }
+
+    public ListNode swapIfLarge(ListNode head) {
+        if(head.next == null||head.next.val>head.val) {
+            return head;
+        }
+        ListNode temp = head.next;
+        head.next = temp.next;
+        temp.next = swapIfLarge(head);
+        return temp;
+    }
+}
