dbfc1b46a1617b0b5486cb17ef8cdf238624f281
DP1110 <manedipak1110@gmail.com>
2026-08-18 19:16:04 +0530

    Time: 8 ms (85.91%), Space: 21 MB (44.09%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -0,0 +1,28 @@
+class BSTIterator(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = []
+        self._push_left(root)
+
+    def _push_left(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        node = self.stack.pop()
+        if node.right:
+            self._push_left(node.right)
+        return node.val
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack) > 0
