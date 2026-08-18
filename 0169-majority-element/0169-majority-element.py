5ca5d0a3f4ef7803097be09aa9caf08d15cd221a
DP1110 <manedipak1110@gmail.com>
2026-08-18 19:12:04 +0530

    Time: 8 ms (62.53%), Space: 13.6 MB (73.85%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        candidate = None
+
+        for n in nums:
+            if count == 0:
+                candidate = n
+            count += 1 if n == candidate else -1
+
+        return candidate
