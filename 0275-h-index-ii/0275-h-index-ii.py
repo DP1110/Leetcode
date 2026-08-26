3bb7d39adff0e75cd7957309b41e9dcb5cfe5872
DP1110 <manedipak1110@gmail.com>
2026-08-26 09:15:12 +0530

    Time: 0 ms (100%), Space: 14.9 MB (62.78%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hIndex(self, citations):
+        n = len(citations)
+        lo, hi = 0, n - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if citations[mid] >= n - mid:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return n - lo
