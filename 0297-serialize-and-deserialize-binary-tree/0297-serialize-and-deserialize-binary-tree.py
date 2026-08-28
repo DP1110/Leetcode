014770ca3f53501143b3840c857c5e1c34c62a4c
DP1110 <manedipak1110@gmail.com>
2026-08-28 20:08:17 +0530

    Time: 87 ms (66.23%), Space: 22.7 MB (15.26%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,24 @@
+class Codec:
+    def serialize(self, root):
+        vals = []
+        def dfs(node):
+            if not node:
+                vals.append('#')
+                return
+            vals.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ','.join(vals)
+
+    def deserialize(self, data):
+        vals = iter(data.split(','))
+        def build():
+            v = next(vals)
+            if v == '#':
+                return None
+            node = TreeNode(int(v))
+            node.left = build()
+            node.right = build()
+            return node
+        return build()
