3ebb538dca1913f596bfd9e8147ac859f25ad923
DP1110 <manedipak1110@gmail.com>
2026-08-30 16:08:31 +0530

    Time: 1 ms (58.16%), Space: 12.5 MB (18.44%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        patches = 0
+        miss = 1
+        i = 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patches += 1
+        return patches
