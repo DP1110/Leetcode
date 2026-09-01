a4bf35a7e0839409c2632243a54bf199024a5e43
DP1110 <manedipak1110@gmail.com>
2026-09-01 08:45:29 +0530

    Time: 0 ms (100%), Space: 12.5 MB (66.65%) - LeetHub - Time: O(n + m), Space: O(n + m)

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        return list(set(nums1) & set(nums2))
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
