b8ed93e54792d88138726b829b9400681d7271de
DP1110 <manedipak1110@gmail.com>
2026-09-08 16:08:13 +0530

    Time: 103 ms (34.52%), Space: 38.5 MB (77.2%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,27 @@
+class Trie(object):
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+    def insert(self, word):
+        node = self
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.is_end = True
+
+    def search(self, word):
+        node = self._find(word)
+        return node is not None and node.is_end
+
+    def startsWith(self, prefix):
+        return self._find(prefix) is not None
+
+    def _find(self, s):
+        node = self
+        for ch in s:
+            if ch not in node.children:
+                return None
+            node = node.children[ch]
+        return node
