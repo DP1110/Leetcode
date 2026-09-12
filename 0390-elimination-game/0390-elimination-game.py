4de87ffa13830cccda176da3f129755292211e5c
DP1110 <manedipak1110@gmail.com>
2026-09-12 12:17:56 +0530

    Time: 3 ms (78.95%), Space: 12.5 MB (19.84%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lastRemaining(self, n):
+        head = 1
+        step = 1
+        left = True
+        remaining = n
+        while remaining > 1:
+            if left or remaining % 2 == 1:
+                head += step
+            step *= 2
+            remaining //= 2
+            left = not left
+        return head
