f33974527ad16437e15a20df5bab19da46639b5b
DP1110 <manedipak1110@gmail.com>
2026-09-12 12:13:21 +0530

    Time: 15 ms (64.83%), Space: 19.5 MB (20.76%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lexicalOrder(self, n):
+        res = []
+        cur = 1
+        for _ in range(n):
+            res.append(cur)
+            if cur * 10 <= n:
+                cur *= 10
+            else:
+                while cur % 10 == 9 or cur + 1 > n:
+                    cur //= 10
+                cur += 1
+        return res
