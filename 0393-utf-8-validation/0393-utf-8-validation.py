eb4845d967b9a998f453cbfd258efaa123e31218
DP1110 <manedipak1110@gmail.com>
2026-09-13 07:07:18 +0530

    Time: 7 ms (21.82%), Space: 12.6 MB (31.82%) - LeetHub

---
+++ 0393-utf-8-validation/0393-utf-8-validation.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def validUtf8(self, data):
+        i = 0
+        n = len(data)
+        while i < n:
+            byte = data[i]
+            if byte >> 7 == 0:
+                num_bytes = 1
+            elif byte >> 5 == 0b110:
+                num_bytes = 2
+            elif byte >> 4 == 0b1110:
+                num_bytes = 3
+            elif byte >> 3 == 0b11110:
+                num_bytes = 4
+            else:
+                return False
+
+            if i + num_bytes > n:
+                return False
+            for j in range(1, num_bytes):
+                if data[i + j] >> 6 != 0b10:
+                    return False
+            i += num_bytes
+        return True
